7809cb2008ddd4b9bd28b9ba9606a8864daa40c3
Benjamin Masor <benjaminmasor@gmail.com>
2022-09-13 20:01:44 -0500

    refactored

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Musix Match API Consuming Site
+
+## Stack
+
+- MongoDB
+- Express.js
+- Node.js
+- React.js
+
+## How to Install
+
+`npm i`
+
+## How to start
+
+`npm start`
+
+## Product Requirements
+
+- allow login & register
+- show the top 3 charting artists for the user country
+- show the last 3 albums for those artists
+- show the last 3 tracks for those albums
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import { authController } from '../controllers/auth';
+import { checkAuthentication } from '../middlewares/auth';
 
 const authRoutes = Router();
 
 authRoutes
   .post('/login', authController.login)
-  .post('/register', authController.register);
+  .post('/register', authController.register)
+  .get('/current_user', checkAuthentication, authController.getCurrentUser);
 
 export { authRoutes };
